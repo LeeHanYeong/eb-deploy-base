29e726f02a346e8d92dc6022137ac665dedcd4d4
LeeHanYeong <dev@lhy.kr>
2019-08-25 19:09:53 +0900

    mashup설정 추가

---
+++ .config/mashup/gunicorn.py
@@ -0,0 +1,13 @@
+daemon = False
+chdir = '/srv/mashup/app'
+bind = 'unix:/tmp/mashup.sock'
+workers = 2
+threads = 1
+timeout = 60
+accesslog = '/var/log/gunicorn/mashup.log'
+errorlog = '/var/log/gunicorn/mashup-error.log'
+capture_output = True
+raw_env = [
+    'DJANGO_SETTINGS_MODULE=config.settings.production',
+]
+pythonpath = '/srv/env-mashup'
