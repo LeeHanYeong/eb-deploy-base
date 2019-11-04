d77a54915863c50a060d53729a251f028142f0db
이한영 <dev@lhy.kr>
2019-11-04 22:22:45 +0900

    update

---
+++ .config/washble/gunicorn.py
@@ -0,0 +1,13 @@
+daemon = False
+chdir = '/srv/washble/app'
+bind = 'unix:/tmp/washble.sock'
+workers = 2
+threads = 1
+timeout = 60
+accesslog = '/var/log/gunicorn/washble.log'
+errorlog = '/var/log/gunicorn/washble-error.log'
+capture_output = True
+raw_env = [
+    'DJANGO_SETTINGS_MODULE=config.settings.dev',
+]
+pythonpath = '/srv/env-washble'
