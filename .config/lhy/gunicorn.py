3448a39077d49611613d7f2e38753f98534db722
이한영 <dev@lhy.kr>
2019-12-05 22:15:21 +0900

    lhy프로젝트 추가

---
+++ .config/lhy/gunicorn.py
@@ -0,0 +1,13 @@
+daemon = False
+chdir = '/srv/lhy/app'
+bind = 'unix:/tmp/lhy.sock'
+workers = 2
+threads = 1
+timeout = 60
+accesslog = '/var/log/gunicorn/lhy.log'
+errorlog = '/var/log/gunicorn/lhy-error.log'
+capture_output = True
+raw_env = [
+    'DJANGO_SETTINGS_MODULE=config.settings.production',
+]
+pythonpath = '/srv/env-lhy'
