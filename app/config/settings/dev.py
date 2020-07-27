83fef5be0db14161c30dddce0005bc809bd4fb2c
lhy <dev@lhy.kr>
2020-07-27 13:54:01 +0900

    projects에서 gunicorn삭제

--- app/config/settings/dev.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-
-DEBUG = True
-AWS_SECRETS_MANAGER_SECRETS_SECTION = 'eb-deploy-base:dev'
-ALLOWED_HOSTS += ['*']
-WSGI_APPLICATION = 'config.wsgi.dev.application'
--- app/config/settings/production.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-
-AWS_SECRETS_MANAGER_SECRETS_SECTION = 'eb-deploy-base:production'
-ALLOWED_HOSTS += SECRETS['ALLOWED_HOSTS']
-WSGI_APPLICATION = 'config.wsgi.production.application'
